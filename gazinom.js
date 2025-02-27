06a48245bb76d66862d09309200514b39c782a62
smhgzi <semihgazi0@icloud.com>
2025-02-27 12:28:47 +0300

    a

--- gazinom.js
+++ gazinom.js
@@ -20,7 +20,48 @@ function waitForElm(selector) {
 
 
 waitForElm('.home-top-games').then((elm) => {
-    const newHTMLContent = `DENEME
+    const newHTMLContent = `
+
+    <div class="home-top-casino" id="external-11aa">
+    <div class="top-section banner">
+        <div class="row-title">Özel ve Öne Çıkanlar</div>
+        <div class="slide-bars">
+            <div class="slide-arrow-box left">
+              <i class="sb-icon sb-arrow-right rotate"></i>
+            </div>
+            <div class="slide-arrow-box right">
+              <i class="sb-icon sb-arrow-right"></i>
+            </div>
+          </div>
+    </div>
+
+
+    <div class="home-top-casino-items">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+    </div>
+
+    <style>
+
+        #external-11aa>.home-top-casino-items{
+            width: 100%;
+            height: auto;
+        }
+
+        #external-11aa>.home-top-casino-items>.card{
+            width: 33%;
+            height: 15vh;
+            background-color: blue;
+        }
+
+    </style>
+</div>
+
+
 `
 
 
