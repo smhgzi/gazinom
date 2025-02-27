897ec5dfdd56423b10d21d448a9a12102190d75c
smhgzi <semihgazi0@icloud.com>
2025-02-27 13:20:44 +0300

    a

--- gazinom.js
+++ gazinom.js
@@ -23,8 +23,119 @@ waitForElm('.home-top-games').then((elm) => {
     const newHTMLContent = `
     
     
+    <div class="home-top-casino" id="external-12aa">
+    <div class="top-section banner">
+        <div class="row-title">Özel ve Öne Çıkanlar</div>
+        <div class="slide-bars">
+            <div class="slide-arrow-box left">
+              <i class="sb-icon sb-arrow-right rotate"></i>
+            </div>
+            <div class="slide-arrow-box right">
+              <i class="sb-icon sb-arrow-right"></i>
+            </div>
+          </div>
+    </div>
+
+
+    <div class="home-top-casino-items">
+        <div class="slider-container">
+            <div class="container">
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                   
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                   
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div><div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                   
+                </div>
+                <div class="container-box">
+                    
+                </div>
+                <div class="container-box">
+                    
+                </div>
+            </div>
+            </div>
+    </div>
+
+    <style>
+
+
+        #external-12aa >div> .slider-container {
+            width: 100%;
+            height: 15vh;
+            overflow: hidden;
+          }
+          
+          #external-12aa > div>.slider-container .container {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-flow: column wrap;
+          }
+          
+          #external-12aa >div .container .container-box {
+            width: 16%;
+            height: 100%;
+            margin-right: 1%;
+            background: rgb(52, 152, 219);
+            color: rgb(41, 128, 185);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius:10px;
+          }
+
+
+    </style>
+
+
     
-    <div class="home-top-casino" id="external-11aa">
+</div>
+
+
+<div class="home-top-casino" id="external-11aa">
     <div class="top-section banner">
         <div class="row-title">Özel ve Öne Çıkanlar</div>
         <div class="slide-bars">
@@ -97,8 +208,9 @@ waitForElm('.home-top-games').then((elm) => {
     </style>
 
 
-   
+    
 </div>
+
 
 `
 
@@ -112,7 +224,7 @@ newElement.classList.add('x');
 homepage.insertBefore(newElement,anchorElement);
 
 
-/*slider*/
+/*slider özel öne çıkanlar*/
 const container = document.querySelector('#external-11aa>div.home-top-casino-items>div>div');
 const next = document.querySelector('#external-11aa>div.top-section.banner>div.slide-bars>.slide-arrow-box.right');
 const prev = document.querySelector('#external-11aa>div.top-section.banner>div.slide-bars>.slide-arrow-box.left');
@@ -138,6 +250,37 @@ function prevElement() {
 
 next.addEventListener('click', nextElement, false);
 prev.addEventListener('click', prevElement, false);
+
+/*slider özel 12aa*/
+const container = document.querySelector('#external-12aa>div.home-top-casino-items>div>div');
+const next = document.querySelector('#external-12aa>div.top-section.banner>div.slide-bars>.slide-arrow-box.right');
+const prev = document.querySelector('#external-12aa>div.top-section.banner>div.slide-bars>.slide-arrow-box.left');
+const size = (container.clientWidth);
+let counter = 0;
+
+function nextElement() {
+  if (counter < 3) {
+    container.style.transition = '.5s ease-in-out';
+    counter++;
+    container.style.transform = 'translateX('+ (-size  * counter) +'px)';
+  }
+	
+}
+
+function prevElement() {
+  if (counter > 0 ) {
+    container.style.transition = '.5s ease-in-out';
+    counter--;
+    container.style.transform = 'translateX('+ (-size  * counter - size*0.01) +'px)';
+  }
+}
+
+next.addEventListener('click', nextElement, false);
+prev.addEventListener('click', prevElement, false);
+
+
+
+
 
 
 console.log("gazinom.js")
